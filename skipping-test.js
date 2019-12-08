f519b59c8f4f00e0e5a877998383d301dcbd2e44
Chris Sears <34785800+OmegaLimit@users.noreply.github.com>
2019-12-07 23:49:32 -0500

    Add files via upload

---
+++ skipping-test.js
@@ -0,0 +1,59 @@
+var expectedValues;
+var cutoffs;
+
+/**
+ * This is the main function.
+ */
+function runSimulation() {
+  
+  expectedValues = [];
+  cutoffs = [];
+  
+  for(var cutoff = 1; cutoff < 100; cutoff++)
+    simulate(cutoff);
+	
+	outputResults();
+}
+
+function simulate(cutoff) {
+  
+  console.log("Simulating: " + cutoff)
+  
+  var trialPresses = [];
+	
+	for(var i = 0; i < 1000000; i++) {
+		var track = randomTrack(100);
+		var presses = 0;
+		while(track !== 0) {
+			presses++;
+			if(track < cutoff) track--;
+			else track = randomTrack(100);
+		}
+		trialPresses.push(presses)
+	}
+
+  var sum = 0;
+  for(i = 0; i < trialPresses.length; i++) {
+    sum += trialPresses[i];
+  }
+
+	expectedValues.push(sum/trialPresses.length);
+	cutoffs.push(cutoff)
+}
+
+function randomTrack(n) { return Math.floor(Math.random() * n); }
+
+/**
+ * This function outputs the results.
+ */
+
+function outputResults() {
+  
+	var output = "<table><tr><th>Cutoff</th><th>Expected Button Pushes</th></tr>"
+	
+	for(var i = 0; i < cutoffs.length; i++) {
+		output += "<tr><td>" + cutoffs[i] + "</td><td>" + expectedValues[i] + "</td></tr>";
+	}
+	output += "</table>"
+	document.getElementById("results").innerHTML = output;
+}
